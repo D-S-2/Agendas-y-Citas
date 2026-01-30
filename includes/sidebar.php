5789372443938b4cd67c5faf009508b86f7c6966
Kebien1 <nunezortizkevin@gmail.com>
2026-01-30 12:26:47 -0400

    s

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,7 +3,7 @@
         <ul>
             <li class="menu-title">Gestión de Citas</li>
             <li><a href="../citas/calendario.php"><i class="far fa-calendar-alt"></i> Calendario de Citas</a></li>
-            
+            <li><a href="../citas/agenda_dia.php"><i class="fas fa-calendar-day"></i> Agenda del Día</a></li>
         </ul>
     </nav>
 </aside>
